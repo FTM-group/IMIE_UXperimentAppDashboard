148eb025390767cc3718a542d2908d3145fdb0d7
Julien <rouillardjulien1@gmail.com>
2019-07-05 11:30:37 +0200

    mise en place du graphique

--- src/GraphPage.js
+++ src/GraphPage.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import Navbar from './Components/Navbar/Navbar';
-import Container from './Components/Container/Container';
 import Graph from './Components/Graph/Graph';
 
 import './App.css';
+import './GraphPage.css';
 import './assets/css/argon.min.css';
 import './assets/icons/icofont.min.css';
 
@@ -12,8 +12,8 @@ function GraphPage() {
     <div>
       <Navbar />
       <main>
-        <div className="position-relative">
-          <section className="section section-lg section-hero section-shaped">
+         <div className="position-relative">
+          { /*<section className="section section-lg section-hero section-shaped">
             <div className="shape shape-style-1 shape-primary">
               <span className="span-150" />
               <span className="span-50" />
@@ -25,18 +25,20 @@ function GraphPage() {
               <span className="span-100" />
               <span className="span-50" />
               <span className="span-100" />
-            </div>
-            <div className="separator separator-bottom separator-skew">
+            </div> */}
+            {/* <div className="separator separator-bottom separator-skew">
               <svg x="0" y="0" viewBox="0 0 2560 100" preserveAspectRatio="none" version="1.1" xmlns="http://www.w3.org/2000/svg">
                 <polygon className="fill-white" points="2560 0 2560 100 0 100" />
               </svg>
             </div>
-          </section>
+          </section> */}
+          <Graph />
         </div>
       </main>
-      <Graph />
+      
     </div>
   );
 } 
+
 
 export default GraphPage;
